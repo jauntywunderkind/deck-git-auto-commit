aeb51f737ec47c068c3cb59f62a38b99399c2eb7
jauntywunderkind <jaunty+wunder+kind+scm@eldergods.com>
2020-04-29 11:27:33 -0400

    cha cha channggess

--- src/index.js
+++ src/index.js
@@ -212,13 +212,13 @@ const Presentation = () => (
 		<Slide>
 			<Markdown>
 				{`
-				1. \`node.js\` - generally my style
-				1. \`isomoprhic-git\` - a native impl of git for js
-				1. \`fs.Watch\` - the general tool for watching files
-				  1. but difficult to do recursively
-				  1. still requires filtering
-				  1. ...some other technical concerns
-				  1. which brings me to...
+					1. \`node.js\` - generally my style
+					1. \`isomoprhic-git\` - a native impl of git for js
+					1. \`fs.Watch\` - the general tool for watching files
+					    1. but difficult to do recursively
+					    1. still requires filtering
+					    1. ...some other technical concerns
+					    1. which brings me to...
 				`}
 			</Markdown>
 		</Slide>
